36bcb7533f34d7d75b5e30176efc1b7f7acc0c60
Mark <mark.lishman@gmail.com>
2016-07-21 08:01:41 +0100

    Resources loaded from CDN

--- systemjs.config.js
+++ systemjs.config.js
@@ -1,48 +1,65 @@
-/**
- * System configuration for Angular 2 samples
- * Adjust as necessary for your application needs.
- */
 (function(global) {
-    // map tells the System loader where to look for things
-    var map = {
-        'app':                        'app', // 'dist',
-        '@angular':                   'node_modules/@angular',
-        'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
-        'rxjs':                       'node_modules/rxjs'
+
+    var ngVer = '@2.0.0-rc.4';
+    var routerVer = '@3.0.0-beta.1';
+    var formsVer = '@0.2.0';
+    var routerDeprecatedVer = '@2.0.0-rc.2';
+
+    var  map = {
+        'app':                        'app',
+
+        '@angular':                   'https://npmcdn.com/@angular',
+        '@angular/router':            'https://npmcdn.com/@angular/router' + routerVer,
+        '@angular/forms':             'https://npmcdn.com/@angular/forms' + formsVer,
+        '@angular/router-deprecated': 'https://npmcdn.com/@angular/router-deprecated' + routerDeprecatedVer,
+        'angular2-in-memory-web-api': 'https://npmcdn.com/angular2-in-memory-web-api',
+        'rxjs':                       'https://npmcdn.com/rxjs@5.0.0-beta.6',
+        'ts':                         'https://npmcdn.com/plugin-typescript@4.0.10/lib/plugin.js',
+        'typescript':                 'https://npmcdn.com/typescript@1.9.0-dev.20160409/lib/typescript.js',
     };
-    // packages tells the System loader how to load when no filename and/or no extension
+
     var packages = {
-        'app':                        { main: 'main.js',  defaultExtension: 'js' },
+        'app':                        { main: 'main.ts',  defaultExtension: 'ts' },
         'rxjs':                       { defaultExtension: 'js' },
         'angular2-in-memory-web-api': { main: 'index.js', defaultExtension: 'js' },
     };
+
     var ngPackageNames = [
         'common',
         'compiler',
         'core',
-        'forms',
         'http',
         'platform-browser',
         'platform-browser-dynamic',
-        'router',
-        'router-deprecated',
-        'upgrade',
+        'upgrade'
     ];
-    // Individual files (~300 requests):
-    function packIndex(pkgName) {
-        packages['@angular/'+pkgName] = { main: 'index.js', defaultExtension: 'js' };
-    }
-    // Bundled (~40 requests):
-    function packUmd(pkgName) {
+
+    ngPackageNames.forEach(function(pkgName) {
+        map['@angular/'+pkgName] = 'https://npmcdn.com/@angular/' + pkgName + ngVer;
+    });
+
+    ngPackageNames.forEach(function(pkgName) {
         packages['@angular/'+pkgName] = { main: '/bundles/' + pkgName + '.umd.js', defaultExtension: 'js' };
-    }
-    // Most environments should use UMD; some (Karma) need the individual index files
-    var setPackageConfig = System.packageWithIndex ? packIndex : packUmd;
-    // Add package entries for angular packages
-    ngPackageNames.forEach(setPackageConfig);
+    });
+
+    packages['@angular/router'] = { main: 'index.js', defaultExtension: 'js' };
+    packages['@angular/forms'] = { main: 'index.js', defaultExtension: 'js' };
+    packages['@angular/router-deprecated'] = { main: '/bundles/router-deprecated' + '.umd.js', defaultExtension: 'js' };
+
     var config = {
+        transpiler: 'ts',
+        typescriptOptions: {
+            tsconfig: true
+        },
+        meta: {
+            'typescript': {
+                "exports": "ts"
+            }
+        },
         map: map,
         packages: packages
     };
+
     System.config(config);
+
 })(this);
